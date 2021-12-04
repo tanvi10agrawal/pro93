ea4868bc25359985914360a35ceb43974acd1d18
tanvi10agrawal <77176204+tanvi10agrawal@users.noreply.github.com>
2021-12-04 14:54:43 -0600

    Add files via upload

---
+++ screens/CityScreen.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default class CityScreen extends Component{
+  render(){
+    return(
+      <View>
+        <Text> welcome to city screen </Text>
+      </View>
+    )
+  }
+}
